82ca35a5455b437b35acfc497dc9ade0641308c7
giahuy1379 <giahuyplaygame@gmail.com>
2018-09-04 16:54:19 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <form role="form" method="POST" action="/home">
+            {{ csrf_field() }}
+            <div class="form-group {{ $errors->has('email') ? 'has-error' : '' }}">
+                <input id="2fa" type="text" class="form-control" name="2fa" placeholder="Enter the code you received here." required autofocus>
+                @if ($errors->has('2fa'))
+                    <span class="help-block">
+<strong>{{ $errors->first('2fa') }}</strong>
+</span>
+                @endif
+            </div>
+            <div class="form-group">
+                <button class="btn btn-primary" type="submit">Send</button>
+            </div>
+        </form>
+    </div>
+@endsection
